ea8be12e329b30ad7ef08b87cc89658e958b86fb
monyu-01 <opsu.0102@gmail.com>
2025-06-03 07:26:18 +0000

    Add: 1デプロイ目に必要なルーティングを設定（homes、members、posts、sessions、registrations）

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,39 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/followings_posts'
-    get 'posts/edit'
-  end
-  namespace :public do
-    get 'members/show'
-    get 'members/dashboard'
-    get 'members/edit_profile'
-    get 'members/saved_posts'
-    get 'members/followers'
-    get 'members/followings'
-    get 'members/edit_information'
-    get 'members/unsubscribe'
-  end
+
+  devise_for :admins
+
   namespace :public do
     get 'sessions/new'
   end
-  namespace :public do
-    get 'registrations/new'
+
+  devise_for :members, controllers: {
+  registrations: 'public/registrations',
+  sessions: 'public/sessions'
+}
+  
+  scope module: :public do
+    root 'homes#top'
+
+    get 'how_to', to: 'homes#how_to'
+    get '/members/dashboard', to: 'members#dashboard',as: 'dashboard'
+    get '/members/edit_profile', to: 'members#edit_profile', as: 'edit_profile'
+    patch '/members/update_profile', to: 'members#update_profile', as: 'update_profile'
+    get '/members/information/edit', to: 'members#edit_information', as: 'edit_information'
+    patch '/members/update_information',to: 'members#update_information', as: 'update_information'
+    get '/members/unsubscribe', to: 'members#unsubscribe', as: 'unsubscribe'
+    delete '/members/withdraw', to: 'members#withdraw', as: 'withdraw'
+    get '/members/saved_posts', to: 'members#saved_posts', as: 'saved_posts'
+
+    resources :members, only: [:show] do
+      member do
+        get :followers          
+        get :followings         
+      end
+    end
+    resources :posts do
+      collection do
+        get :followings
+      end
+    end
   end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/how_to'
-  end
-  devise_for :admins
-  devise_for :members
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
