4e0e04da3e48f59eb6e4cf29edc202a02b52a6cf
monyu-01 <opsu.0102@gmail.com>
2025-06-03 05:55:24 +0000

    Add: Postモデルの作成とマイグレーション適用（1デプロイ目の基礎構築）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_03_030613) do
+ActiveRecord::Schema.define(version: 2025_06_03_054452) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,10 +30,22 @@ ActiveRecord::Schema.define(version: 2025_06_03_030613) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name", null: false
+    t.text "self_introduction"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.integer "member_id", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["member_id"], name: "index_posts_on_member_id"
+  end
+
+  add_foreign_key "posts", "members"
 end
