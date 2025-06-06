8340d6222c2124508a4f08a72a7df4d96769b78a
monyu-01 <opsu.0102@gmail.com>
2025-06-06 11:40:17 +0000

    Add: マイページ画面を実装

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,7 +1,8 @@
 class Public::MembersController < ApplicationController
   def show
-    #@member = Member.find(params[:id])
-    #@post = @member.post  
+    @member = Member.find(params[:id])
+    @post = Post.find(params[:id])
+    @posts = @member.posts
   end
 
 
@@ -11,8 +12,10 @@ class Public::MembersController < ApplicationController
 
   def update_profile
     @member = current_member
-    if @member.update(profile_params)
-      redirect_to member_path(@member), notice: 'プロフィールを更新しました。'
+    @member.assign_attributes(profile_params)
+    if @member.valid?(:update_profile)
+      @member.save
+      redirect_to member_path(@member)
     else
       render :edit_profile
     end
@@ -30,7 +33,7 @@ class Public::MembersController < ApplicationController
   private
 
   def profile_params
-    params.require(:member).permit(:name, :introduction)
+    params.require(:member).permit(:name, :self_introduction)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,12 @@ class Post < ApplicationRecord
   belongs_to :member
   has_one_attached :image
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg') #仮画像
+      file_path = Rails.root.join('app/assets/images/sample.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg') #仮画像
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
   validates :title,  presence: true
