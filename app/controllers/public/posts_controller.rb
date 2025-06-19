49aef57d1573ee7b8adb9ea50fd38ba0e196e447
monyu-01 <opsu.0102@gmail.com>
2025-06-19 08:18:12 +0000

    Add: フォロー・フォロワー機能のレイアウトを実装

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -39,7 +39,7 @@ class Public::PostsController < ApplicationController
   end
 
   def followings
-    following_ids = crrent_member.following_ids
+    following_ids = current_member.following_ids
     @posts = Post.where(member_id: following_ids).includes(:member, :genre).order(created_at: :desc).page(params[:page])
   end
 
