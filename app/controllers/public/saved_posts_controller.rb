ec5f1916dc5602745cd1155e455e386a37d38dc7
monyu-01 <opsu.0102@gmail.com>
2025-06-23 01:06:44 +0000

    Add: フォロー・フォロワーの非同期処理を実装（Ajax対応）

--- app/controllers/public/saved_posts_controller.rb
+++ app/controllers/public/saved_posts_controller.rb
@@ -5,13 +5,11 @@ class Public::SavedPostsController < ApplicationController
     @post = Post.find(params[:post_id])
     saved_post = current_member.saved_posts.new(post_id: @post.id)
     saved_post.save
-    # redirect_to post_path(post)
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     saved_post = current_member.saved_posts.find_by(post_id: @post.id)
     saved_post.destroy
-    # redirect_to post_path(post)
   end
 end
