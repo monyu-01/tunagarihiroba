8f90959728f337398002aaf8f726351c79b1371c
monyu-01 <opsu.0102@gmail.com>
2025-06-23 09:34:46 +0000

    Add: 管理者側に投稿一覧・詳細・削除機能を追加

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,16 @@
+class Admin::PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:member).order(created_at: :desc).page(params[:page]).per(15)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments.includes(:member).order(created_at: :desc)  
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path, notice: "投稿を削除しました。"
+  end
+end
