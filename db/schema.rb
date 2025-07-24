329298eaa68eb52c579feac945f736644a173473
monyu-01 <opsu.0102@gmail.com>
2025-07-24 09:52:34 +0000

    Fix: フォロー機能用の中間テーブルrelationshipsを再作成し、外部キー制約とユニーク制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_24_102715) do
+ActiveRecord::Schema.define(version: 2025_07_24_084745) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2025_06_24_102715) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -113,6 +113,9 @@ ActiveRecord::Schema.define(version: 2025_06_24_102715) do
     t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -138,6 +141,8 @@ ActiveRecord::Schema.define(version: 2025_06_24_102715) do
   add_foreign_key "comments", "members"
   add_foreign_key "comments", "posts"
   add_foreign_key "posts", "members"
+  add_foreign_key "relationships", "members", column: "followed_id"
+  add_foreign_key "relationships", "members", column: "follower_id"
   add_foreign_key "saved_posts", "members"
   add_foreign_key "saved_posts", "posts"
 end
